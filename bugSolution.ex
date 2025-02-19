1c747dc9b61f957029481af4adf54b8ac022b24a
BugHunter-X <askzenai@gmail.com>
2025-02-19 03:26:05 +0100

    Add bugSolution.ex file

---
+++ bugSolution.ex
@@ -0,0 +1,14 @@
+```elixir
+list = [1, 2, 3, 4, 5]
+
+try do
+  Enum.each(list, fn x ->
+    if x == 3 do
+      throw(:stop_iteration)
+    end
+    IO.puts(x)
+  end)
+catch
+  :stop_iteration -> IO.puts("Iteration stopped")
+end
+```
